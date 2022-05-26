13bd5109322982efce3b2f61cd58d20536d8256d
feratyusa <prabuega55@gmail.com>
2022-05-26 12:36:56 +0700

    delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcrypt");
-
-const text = "butahuruf";
-const textC = bcrypt.hashSync(text, 10);
-const textD = "butahuruf";
-const match = bcrypt.compareSync(textD, textC);
-
-console.log(match);
